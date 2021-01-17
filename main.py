819ea090ef860b8fa9faf961a43d142c606d86cb
Vlad <vdzhabarov@tractionguest.com>
2021-01-17 14:13:03 +0200

    Convert to use /threadmarks page exclusively for TOC

--- main.py
+++ main.py
@@ -1,52 +1,32 @@
 #!/usr/bin/env python
 
 from requests import get
-from feedparser import parse
 from pprint import pprint
 from bs4 import BeautifulSoup
+from urllib import request
 
-all_chapter_metadata = []
-all_chapters = []
+all_chapter_data = []
 
 
 def get_html(url):
-    reader_mode_url = "{}/reader".format(url)
-    content = get(reader_mode_url).content.decode('utf-8')
-    return content
+    response = get(url)
+    html = response.content.decode('utf-8')
+    return html
 
 
-def get_post(soup, post):
-    post_filter = {
-        "data-lb-id": post
-    }
-    message = soup.findAll("div", post_filter)
+def get_post_text(url):
+    soup = get_soup(url)
+    body = soup.findAll("div", {"class": "bbWrapper"})
 
-    if message:
-        print("Parsing post ", post)
-        message = message[0].findAll("div", {"class": "bbWrapper"})[0]
-        message = str(message)
-        return message
-    return 'none'
+    if len(body) > 0:
+        return str(body[0])
+    return 'Could not download chapter text'
 
 
 def get_soup(url):
     html = get_html(url)
     soup = BeautifulSoup(html, 'html.parser')
     return soup
-
-
-def get_message(soup, chapter, retries=0):
-    if retries < 2:
-        message = get_post(soup, chapter['post'])
-
-        if message != 'none':
-            return message
-        else:
-            retries += 1
-            soup = get_soup(chapter['link'])
-            get_message(soup, chapter, retries)
-    else:
-        return 'skip'
 
 
 def save_file(filename, output):
@@ -56,29 +36,18 @@ def save_file(filename, output):
     f.close()
 
 
-# Parse threadmarks RSS to grab some metadata about every chapter
-# Chapter threadmarks are in reverse order with latest entries first, so we want to flip this when saving to a file
-def get_chapter_metadata(rss):
-    entries = rss.entries
-    entries.reverse()
-    for entry in entries:
-        post = entry.link.split('/')[-1]
-        chapter = {
-            'title': entry.title,
-            'description': entry.description,
-            'link': entry.link,
-            'post': post
-        }
-        all_chapter_metadata.append(chapter)
+def build_html_base(url, metadata):
+    soup = get_soup(url)
+    head = str(soup.head)
+    title = metadata['title']
+    author = metadata['author']
 
-
-def build_html_base(head, title, author):
     fic_header_html = """
 <h1>{title}</h1>
 <h2>By: {author}</h2>
 """.format(title=title, author=author)
 
-    base = str(head) + fic_header_html
+    base = head + fic_header_html
     return base
 
 
@@ -87,44 +56,85 @@ def build_chapter_html(chapter):
 <hr>
 <br>
 <h2>{name}</h2>
-<h4>{description}</h4>
 <br><br>
-{content}
+{body}
 <br><br>
-""".format(name=chapter['title'], description=chapter['description'], content=chapter['message'])
+""".format(name=chapter['title'], body=chapter['body'])
     return chapter_html
 
 
+def build_filename(url):
+    parsed = request.urlparse(url)
+    # Splitting this into multiple lines for readability
+    thread_name = parsed.path.rstrip('/').split('/')[-1]    # Remove slashes from path
+    thread_name = thread_name.split('.')[0]                 # Remove numeric thread ID
+    return thread_name + '.html'
+
+
+# Generate base forum URL from full link
+def get_base_url(url):
+    parsed = request.urlparse(url)
+    base_url = parsed.scheme + '://' + parsed.netloc
+    return base_url
+
+
+def get_story_metadata(threadmarks_url):
+    soup = get_soup(threadmarks_url)
+    base_url = get_base_url(threadmarks_url)
+    metadata = {}
+
+    metadata['title'] = soup.findAll("h1", {"class": "threadmarkListingHeader-name"})[0].get_text()
+
+    description_soup = soup.findAll("article", {"class": "threadmarkListingHeader-extraInfoChild message-body"})[0]
+    metadata['description'] = description_soup.findAll("div", {"class": "bbWrapper"})[0].get_text()
+
+    author = soup.findAll("a", {"class": "username"})[0]
+    metadata['author'] = author.get_text()
+    metadata['author_profile'] = base_url + str(author.get("href"))
+
+    return metadata
+
+
+def parse_threadmarks(threadmarks_url):
+    base_url = get_base_url(threadmarks_url)
+    soup = get_soup(threadmarks_url)
+
+    # This is the closest HTML structure to the threadmark link that I can search for without pulling in extra elements
+    soup = soup.findAll("div", {"class": "structItem-title threadmark_depth0"})
+
+    for chapter in soup:
+        chapter_metadata = {}
+        link = chapter.findAll("a")[0]  # Surprisingly this is way faster than calling .a method on chapter soup
+        path = link.get("data-preview-url")   # Opens preview URL so there's less stuff to load
+        chapter_metadata['title'] = link.get_text()
+
+        print(chapter_metadata['title'])
+
+        chapter_metadata['url'] = base_url + path
+        all_chapter_data.append(chapter_metadata)
+
+
 def main():
-    thread_url = input("Enter URL to the story you wish to download: ").rstrip('/')
-    rss_url = "{}/threadmarks.rss".format(thread_url)
-    filename = thread_url.split('/')[-1].split('.')[0] + '.html'
+    # thread_url = input("Enter URL to the story you wish to download: ").rstrip('/')
+    thread_url = "https://forums.spacebattles.com/threads/empress-in-azeroth-drowtales-warcraft-crossover.905910"
+    thread_url = "https://forums.spacebattles.com/threads/the-last-angel.244209"
+
+    filename = build_filename(thread_url)
     print(filename)
-    print(thread_url)
 
-    print("Downloading Threadmarks RSS at {}".format(rss_url))
-    rss = parse(rss_url)
-    threadmarks = rss.entries
+    threadmarks_url = thread_url + '/threadmarks'
+    parse_threadmarks(threadmarks_url)
 
-    if len(threadmarks) < 1:
-        raise Exception("NoThreadmarksError: No threadmarks found for given thread. Cannot download.")
-    print("Found {} chapters in story".format(len(threadmarks)))
+    story_metadata = get_story_metadata(threadmarks_url)
 
-    print("Parsing RSS for chapter metadata.")
-    get_chapter_metadata(rss)
-    soup = get_soup(all_chapter_metadata[0]['link'])
-
-    html_base = build_html_base(head=soup.head, title=rss.feed.title, author=rss.entries[-1].author)
+    html_base = build_html_base(thread_url, story_metadata)
     html_body = ""
 
-    for chapter in all_chapter_metadata:
+    for chapter in all_chapter_data:
         print('Downloading story content for chapter titled "{}"'.format(chapter['title']))
-        message = get_message(soup, chapter)
-        if message != 'skip':
-            chapter['message'] = message
-            all_chapters.append(chapter)
+        chapter['body'] = get_post_text(chapter['url'])
 
-    for chapter in all_chapters:
+    for chapter in all_chapter_data:
         html_body += build_chapter_html(chapter)
 
     output = html_base + html_body
