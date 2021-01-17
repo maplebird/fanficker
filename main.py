7e3b9f7290a8143a944c0564e9b264534cac4221
Vlad <vdzhabarov@tractionguest.com>
2021-01-17 18:08:19 +0200

    Add CSS, move HTML generation to template

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 from requests import get
-from pprint import pprint
 from bs4 import BeautifulSoup
 from urllib import request
+from jinja2 import Template, FileSystemLoader
 
-all_chapter_data = []
 
 
 def get_html(url):
@@ -44,61 +43,70 @@ def build_filename(url):
     return thread_name + '.html'
 
 
-def build_html_base(url, metadata):
-    soup = get_soup(url)
-    head = str(soup.head)
-    title = metadata['title']
-    author = metadata['author']
+def build_html(metadata, chapters):
+    with open('template.j2.html') as f:
+        template = Template(f.read())
 
-    fic_header_html = """
-<h1>{title}</h1>
-<h2>By: {author}</h2>
-""".format(title=title, author=author)
+    html = template.render(
+        metadata=metadata,
+        chapters=chapters
+    )
 
-    base = head + fic_header_html
-    return base
+    return html
 
 
-def build_table_of_contents(all_chapter_data):
-    toc = """
-<h2>Table of Contents</h2>
-"""
-    counter = 0
-    for chapter in all_chapter_data:
-        bookmark = '\n<li><a href="#bookmark{counter}">{name}</a></li>'.format(
-            counter=counter, name=chapter['title']
-        )
-        toc += bookmark
-        counter += 1
 
-    return toc
-
-
-def build_chapter_html(chapter, counter=0):
-    chapter_html = """
-<hr>
-<br>
-<h2 id="bookmark{counter}">{name}</h2>
-<br><br>
-{body}
-<br><br>
-""".format(counter=counter, name=chapter['title'], body=chapter['body'])
-    return chapter_html
+# def build_html_base(url, metadata):
+#     soup = get_soup(url)
+#     head = str(soup.head)
+#     title = metadata['title']
+#     author = metadata['author']
+#
+#     fic_header_html = """
+# <h1>{title}</h1>
+# <h2>By: {author}</h2>
+# """.format(title=title, author=author)
+#
+#     base = head + fic_header_html
+#     return base
 
 
-def build_story_body(all_chapter_data):
-    body = ""
+# def build_table_of_contents(chapters):
+#     toc = """
+# <ul class="toc">
+# <h2>Table of Contents</h2>
+# """
+#     # Arrays start at 1. Fight me.
+#     counter = 1
+#     for chapter in chapters:
+#         bookmark = '\n<li><a href="#threadmark-{counter}">{name}</a></li>'.format(
+#             counter=counter, name=chapter['title']
+#         )
+#         toc += bookmark
+#         counter += 1
+#
+#     toc += '\n</ul>'
+#
+#     return toc
 
-    for chapter in all_chapter_data:
+#
+# def build_chapter_html(chapter, counter=1):
+#     chapter_html = """
+# <hr><br>
+# <h2 id="threadmark-{counter}">{name}</h2>
+# <br><br>
+# {body}
+# <br><br>
+# """.format(counter=counter, name=chapter['title'], body=chapter['body'])
+#     return chapter_html
+
+# Mutates chapters var by adding chapter body for each.
+def build_story_body(chapters):
+    for chapter in chapters:
         print('Downloading story content for chapter titled "{}"'.format(chapter['title']))
         chapter['body'] = get_post_text(chapter['url'])
 
-    counter = 0
-    for chapter in all_chapter_data:
-        body += build_chapter_html(chapter, counter)
-        counter += 1
-
-    return body
+    return chapters
 
 
 # Generate base forum URL from full link
@@ -128,41 +136,45 @@ def get_story_metadata(threadmarks_url):
 def parse_threadmarks(threadmarks_url):
     base_url = get_base_url(threadmarks_url)
     soup = get_soup(threadmarks_url)
+    chapters = []
 
     # This is the closest HTML structure to the threadmark link that I can search for without pulling in extra elements
     soup = soup.findAll("div", {"class": "structItem-title threadmark_depth0"})
 
+    counter = 1
     for chapter in soup:
         chapter_metadata = {}
-        link = chapter.findAll("a")[0]  # Surprisingly this is way faster than calling .a method on chapter soup
-        path = link.get("data-preview-url")   # Opens preview URL so there's less stuff to load
+        link = chapter.findAll("a")[0]              # Surprisingly this is way faster than calling .a method on chapter soup
+        path = link.get("data-preview-url")         # Opens preview URL so there's less stuff to load
+
         chapter_metadata['title'] = link.get_text()
-
-        print(chapter_metadata['title'])
-
         chapter_metadata['url'] = base_url + path
-        all_chapter_data.append(chapter_metadata)
+        chapter_metadata['threadmark'] = counter    # Set up a threadmark counter for ToC
+
+        chapters.append(chapter_metadata)
+        counter +=1
+
+    return chapters
 
 
 def main():
-    # thread_url = input("Enter URL to the story you wish to download: ").rstrip('/')
-    thread_url = "https://forums.spacebattles.com/threads/empress-in-azeroth-drowtales-warcraft-crossover.905910"
-    thread_url = "https://forums.spacebattles.com/threads/the-last-angel.244209"
-
+    thread_url = input("Enter URL to the story you wish to download: ").rstrip('/')
     filename = build_filename(thread_url)
     print(filename)
 
     threadmarks_url = thread_url + '/threadmarks'
-    parse_threadmarks(threadmarks_url)
 
+    chapters = parse_threadmarks(threadmarks_url)
     story_metadata = get_story_metadata(threadmarks_url)
 
-    html_base = build_html_base(thread_url, story_metadata)
-    toc = build_table_of_contents(all_chapter_data)
-    story_body = build_story_body(all_chapter_data
-                                  )
-    output = html_base + toc + story_body
-    save_file(filename, output)
+    # html_base = build_html_base(thread_url, story_metadata)
+    # toc = build_table_of_contents(chapters)
+
+    chapters = build_story_body(chapters)
+
+    html = build_html(story_metadata, chapters)
+
+    save_file(filename, html)
 
 
 if __name__ == "__main__":
